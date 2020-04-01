1bb51a1645de22aface6827f190703732d62bb3f
Emil <>
2020-04-01 07:00:04 +0300

    add TodoByIdController abstract class which all controllers for this resource will inherit from

---
+++ controllers/todo_by_id_controllers/todo_by_id_controller.py
@@ -0,0 +1,16 @@
+import abc
+from controllers.controller import GetController, PutController, DeleteController
+
+
+class TodoByIdController(GetController, PutController, DeleteController):
+    @abc.abstractmethod
+    def get(self, *args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def put(self, *args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def delete(self, *args, **kwargs):
+        pass
