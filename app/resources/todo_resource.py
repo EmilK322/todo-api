645d831f242747414f7b4267bf77685944c59562
Emil <>
2020-04-01 08:44:27 +0300

    use TodoController that injected to resource

--- app/resources/todo_resource.py
+++ app/resources/todo_resource.py
@@ -1,14 +1,17 @@
 from flask_restful import Resource
+from flask import request
+from app.controllers.todo_controllers.todo_controller import TodoController
 
 
 class Todo(Resource):
     def __init__(self, **kwargs):
-        pass
+        self.controller: TodoController = kwargs['controller']
 
     def get(self):
-        # TODO: return all todos
-        raise NotImplementedError()
+        todos = self.controller.get()
+        return todos
 
     def post(self):
-        # TODO: get input, validate it, create new todo, return new_todo
-        raise NotImplementedError()
+        body = request.get_json()
+        new_todo = self.controller.post(**body)
+        return new_todo
