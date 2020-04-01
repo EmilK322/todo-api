8c17fe0c1b574895fbcbf7a93c32131466e04564
Emil <>
2020-04-01 06:49:04 +0300

    initialize flask and flask-restful

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api
+from resources.todo_resource import Todo
+from resources.todo_by_id_resource import TodoById
+
+app = Flask(__name__)
+api = Api(app)
+
+
+api.add_resource(Todo, '/todo')
+api.add_resource(TodoById, '/todo/<int:todo_id>')
+
+if __name__ == '__main__':
+    app.run()
+
+
