192f08ea351cb2d9dbacf15e0a60a5bb680f5915
Emil <>
2020-04-01 07:30:33 +0300

    FuncDTO utility class added for transferring functions with their parameters easily

--- app/common/utils.py
+++ app/common/utils.py
@@ -1,3 +1,10 @@
 def get_self_if_true_or_default(check_obj, default_val):
     ret_val = check_obj if check_obj else default_val
     return ret_val
+
+
+class FuncDTO:
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
