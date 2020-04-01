aea5982c8ed4cd9f1260f4c84579bba1eb23067d
Emil <>
2020-04-01 08:50:06 +0300

    use TodoByIdController that injected to resource

--- app/resources/todo_by_id_resource.py
+++ app/resources/todo_by_id_resource.py
@@ -1,18 +1,21 @@
 from flask_restful import Resource
+from flask import request
+from app.controllers.todo_by_id_controllers.todo_by_id_controller import TodoByIdController
 
 
 class TodoById(Resource):
     def __init__(self, **kwargs):
-        pass
+        self.controller: TodoByIdController = kwargs['controller']
 
     def get(self, todo_id):
-        # TODO: get input(id), validate it, return existing todo
-        raise NotImplementedError()
+        todo = self.controller.get(todo_id)
+        return todo
 
     def put(self, todo_id):
-        # TODO: get input(text, completed), validate it, change the existing todo in db and return changed todo
-        raise NotImplementedError()
+        body = request.get_json()
+        changed_todo = self.controller.put(todo_id, **body)
+        return changed_todo
 
     def delete(self, todo_id):
-        # TODO: get input(id), validate it, delete existing todo, return deleted todo
-        raise NotImplementedError()
+        deleted_todo = self.controller.delete(todo_id)
+        return deleted_todo
