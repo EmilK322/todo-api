38ae3cae559762b6d26e519301ab12c72c58747b
Emil <>
2020-04-01 06:56:52 +0300

    add get,post,put and delete controllers abstract classes based on controller class

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,3 +5,25 @@ class Controller(abc.ABC):
     pass
 
 
+class GetController(Controller):
+    @abc.abstractmethod
+    def get(self, *args, **kwargs):
+        pass
+
+
+class PostController(Controller):
+    @abc.abstractmethod
+    def post(self, *args, **kwargs):
+        pass
+
+
+class PutController(Controller):
+    @abc.abstractmethod
+    def put(self, *args, **kwargs):
+        pass
+
+
+class DeleteController(Controller):
+    @abc.abstractmethod
+    def delete(self, *args, **kwargs):
+        pass
