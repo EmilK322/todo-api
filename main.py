47d9bf97fba58699366204b2a27b0d6f7d8155ff
Emil <>
2020-04-01 08:52:36 +0300

    inject kwargs with controllers to the resources

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from app.resources.todo_resource import Todo
 from app.resources.todo_by_id_resource import TodoById
 from app.common.database import close_session, initialize_db
-
+from app.common.controllers_resources_map_bootstrapper import todo_kwargs, todo_by_id_kwargs
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,8 +14,8 @@ def shutdown_session(exception=None):
     close_session()
 
 
-api.add_resource(Todo, '/todo')
-api.add_resource(TodoById, '/todo/<int:todo_id>')
+api.add_resource(Todo, '/todo', resource_class_kwargs=todo_kwargs)
+api.add_resource(TodoById, '/todo/<int:todo_id>', resource_class_kwargs=todo_by_id_kwargs)
 
 if __name__ == '__main__':
     initialize_db()
