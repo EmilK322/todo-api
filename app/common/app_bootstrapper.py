ccd556ef8deb346ff153f190482980d4de991a29
Emil <>
2020-04-02 14:42:40 +0300

    logging added

--- app/common/app_bootstrapper.py
+++ app/common/app_bootstrapper.py
@@ -1,13 +1,13 @@
 import logging
 from app.common.logger.logging_bootstrapper import bootstrap_logging
 from app.common.database import initialize_db
-logger = logging.getLogger(__name__)
+
+_logger = logging.getLogger(__name__)
 
 
 def bootstrap():
     bootstrap_logging()
-    logger.info('bootstrap logging finished')
-    logger.info('start initializing database')
+    _logger.info('bootstrap logging finished')
+    _logger.info('start initializing database')
     initialize_db()
-    logger.info('finished initializing database')
-
+    _logger.info('finished initializing database')
--- app/common/orm/orm_factory.py
+++ app/common/orm/orm_factory.py
@@ -1,11 +1,21 @@
+import logging
+
+
 class OrmFactory:
     def __init__(self, orm_initializers_map):
         self.orm_initializers_map = orm_initializers_map
+        self._logger = logging.getLogger(__name__)
 
     def get_orm_object(self, orm_name):
+        self._logger.info('trying to get orm initializer based on given orm name')
         initializer = self.orm_initializers_map.get(orm_name)
         if not initializer:
-            raise ValueError(f'the orm name: {orm_name} is not valid or not supported')
+            message = f'the orm name: {orm_name} is not valid or not supported'
+            self._logger.error(f'failed to get orm initializer, {message}')
+            raise ValueError(message)
+
+        self._logger.info('successfully got orm initializer based on given orm name')
+        self._logger.info('return initialize_orm method from orm initializer')
         orm_obj = initializer.initialize_orm()
         return orm_obj
 
@@ -16,7 +26,10 @@ class OrmInitializer:
         self.initializer_func = initializer_func
         self.func_args = func_args
         self.func_kwargs = func_kwargs
+        self._logger = logging.getLogger(__name__)
 
     def initialize_orm(self):
+        self._logger.info('start initializing orm')
         orm_obj = self.initializer_func(*self.func_args, **self.func_kwargs)
+        self._logger.info('finished initializing orm')
         return orm_obj
