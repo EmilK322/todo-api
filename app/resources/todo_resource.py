8ae13f7b28376e4d05e88a1b331c83364907c7ab
Emil <>
2020-04-02 08:31:14 +0300

    logging added

--- app/resources/todo_resource.py
+++ app/resources/todo_resource.py
@@ -1,3 +1,4 @@
+import logging
 from flask_restful import Resource
 from flask import request
 from app.controllers.todo_controllers.todo_controller import TodoController
@@ -5,13 +6,20 @@ from app.controllers.todo_controllers.todo_controller import TodoController
 
 class Todo(Resource):
     def __init__(self, **kwargs):
+        self.logger = logging.getLogger(__name__)
         self.controller: TodoController = kwargs['controller']
 
     def get(self):
+        self.logger.info('calling GET')
+        self.logger.info('calling controller\'s get method')
         todos = self.controller.get()
+        self.logger.info(f'controller finished and returned: {todos}')
         return todos
 
     def post(self):
         body = request.get_json()
+        self.logger.info(f'calling POST with body: {body}')
+        self.logger.info(f'calling controller\'s post method')
         new_todo = self.controller.post(**body)
+        self.logger.info(f'controller finished and returned: {new_todo}')
         return new_todo
