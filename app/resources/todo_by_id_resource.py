eeb48c7f7a1e4a1280eb108157c499937da82c3f
Emil <>
2020-04-02 08:27:10 +0300

    added logging to TodoById resource

--- app/resources/todo_by_id_resource.py
+++ app/resources/todo_by_id_resource.py
@@ -1,3 +1,4 @@
+import logging
 from flask_restful import Resource
 from flask import request
 from app.controllers.todo_by_id_controllers.todo_by_id_controller import TodoByIdController
@@ -5,17 +6,27 @@ from app.controllers.todo_by_id_controllers.todo_by_id_controller import TodoByI
 
 class TodoById(Resource):
     def __init__(self, **kwargs):
+        self.logger = logging.getLogger(__name__)
         self.controller: TodoByIdController = kwargs['controller']
 
     def get(self, todo_id):
+        self.logger.info(f'calling GET with todo_id: {todo_id}')
+        self.logger.info(f'calling controller\'s get method with todo_id: {todo_id}')
         todo = self.controller.get(todo_id)
+        self.logger.info(f'controller finished and returned: {todo}')
         return todo
 
     def put(self, todo_id):
         body = request.get_json()
+        self.logger.info(f'calling PUT with todo_id: {todo_id} and body: {body}')
+        self.logger.info('calling controller\'s put method with todo_id and body')
         changed_todo = self.controller.put(todo_id, **body)
+        self.logger.info(f'controller finished and returned: {changed_todo}')
         return changed_todo
 
     def delete(self, todo_id):
+        self.logger.info(f'calling DELETE with todo_id: {todo_id}')
+        self.logger.info(f'calling controller\'s delete method with todo_id: {todo_id}')
         deleted_todo = self.controller.delete(todo_id)
+        self.logger.info(f'controller finished and returned: {deleted_todo}')
         return deleted_todo
