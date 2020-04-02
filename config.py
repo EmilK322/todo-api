9f0b7b77572eea61d8a22c8278b48aaec10466f3
Emil <>
2020-04-02 07:32:55 +0300

    added configuration for logging

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-import os
+import pathlib
+import logging
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = str(pathlib.Path.cwd())
 
-SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'todo_api.sqlite')}"
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{str(pathlib.Path(BASE_DIR) / 'todo_api.sqlite')}"
 SQLALCHEMY_ENGINE_KWARGS = {
     'convert_unicode': True
 }
@@ -10,3 +11,10 @@ SQLALCHEMY_SESSION_KWARGS = {
     'autocommit': False,
     'autoflush': False
 }
+
+LOGGING_LEVEL = logging.INFO
+
+ROTATING_LOG_FILE_NAME = f"{str(pathlib.Path(BASE_DIR) / 'logs' / 'logfile.log')}"
+ROTATING_LOG_FILE_MAX_BYTES = 2 * 1024 * 1024  # 2MB
+ROTATING_LOG_FILE_BACKUP_COUNT = 5
+ROTATING_LOG_FILE_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
