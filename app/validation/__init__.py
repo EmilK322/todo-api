b45da767f5ff8b781a32df188d82d5f56f555d7a
Emil <>
2020-04-19 20:18:32 +0300

    TodoValidator implementation of Validator using Marshmallow added

---
+++ app/validation/__init__.py
@@ -0,0 +1,2 @@
+from .validation_result import ValidationResult
+from .todo_validator import TodoValidator
---
+++ app/validation/todo_validator.py
@@ -0,0 +1,19 @@
+import logging
+from typing import Mapping, Any
+from .validation_result import ValidationResult
+from app.validation.abc import Validator
+from marshmallow import Schema, ValidationError
+
+
+class TodoValidator(Validator):
+    def __init__(self, todo_schema: Schema):
+        self._todo_schema: Schema = todo_schema
+        self._logger = logging.getLogger(__name__)
+
+    def validate_from_dict(self, input_dict: Mapping[str: Any]) -> ValidationResult:
+        try:
+            self._logger.info(f'trying to load dictionary')
+            todo_obj = self._todo_schema.load(input_dict)
+            return ValidationResult(True, todo_obj)
+        except ValidationError as err:
+            return ValidationResult(False, err)
