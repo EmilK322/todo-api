6ac2b6db20686b923c333d36fc1d88813c8ce7df
Emil <>
2020-04-19 20:24:19 +0300

    deleted orm and database files

--- app/common/orm/orm_object.py
+++
@@ -1,8 +0,0 @@
-class OrmObject:
-    def __init__(self, engine, session, model, db_init_func_dto, session_remove_func_dto, **extra):
-        self.engine = engine
-        self.session = session
-        self.model = model
-        self.db_init_func_dto = db_init_func_dto
-        self.session_remove_func_dto = session_remove_func_dto
-        self.extra = extra
