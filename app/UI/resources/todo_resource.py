ebece21070b42556f7ca5e52247014ab4b1ee5ef
Emil <>
2020-04-20 08:38:08 +0300

    fixed logger variable name

--- app/UI/resources/todo_resource.py
+++ app/UI/resources/todo_resource.py
@@ -10,24 +10,24 @@ from app.BLL.controllers import InvalidArgsError
 
 class TodoResource(Resource):
     def __init__(self, **kwargs):
-        self.logger = logging.getLogger(__name__)
+        self._logger = logging.getLogger(__name__)
         self.controller: TodoController = kwargs['controller']
 
     def get(self):
-        self.logger.info('calling GET')
-        self.logger.info('calling controller\'s get method')
+        self._logger.info('calling GET')
+        self._logger.info('calling controller\'s get method')
         todo_list = self.controller.read()
-        self.logger.info(f'controller finished and returned: {todo_list}')
+        self._logger.info(f'controller finished and returned: {todo_list}')
         self._logger.info(f'return StatusCode: {StatusCode.GENERIC_SUCCESS.value}')
         return todo_list, StatusCode.GENERIC_SUCCESS.value
 
     def post(self):
         body = request.get_json()
-        self.logger.info(f'calling POST with body: {body}')
+        self._logger.info(f'calling POST with body: {body}')
         try:
-            self.logger.info(f'calling controller\'s post method')
+            self._logger.info(f'calling controller\'s post method')
             new_todo = self.controller.create(**body)
-            self.logger.info(f'controller finished and returned: {new_todo}')
+            self._logger.info(f'controller finished and returned: {new_todo}')
             self._logger.info(f'return StatusCode: {StatusCode.SUCCESSFULLY_CREATED.value}')
             return new_todo, StatusCode.SUCCESSFULLY_CREATED.value
         except InvalidArgsError as err:
